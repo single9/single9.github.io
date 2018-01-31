5f61c1a8cad3a1804a854be724911eb03e2b7c0c
Duye Chen <k078264@gmail.com>
2018-01-31 16:38:09 +0800

    Use CSS3 Animation

--- js/index.js
+++ js/index.js
@@ -25,33 +25,16 @@
 
             if ( target in index === false) {
                 return location.hash = "#about";
+            } else {
+                index[target].classList.remove('out');
             }
 
             for (let i in index) {
-                if ( i === target || index[i].classList.contains('uk-hidden')) continue;
+                if ( i === target || index[i].classList.contains('out')) continue;
                 index[i].classList.add('out');
-
-                PrefixedEventAniEnd(index[i], function () {
-                    index[target].classList.remove('out');
-                    index[target].classList.remove('uk-hidden');
-                });
             }
 
             UIkit.offcanvas("#offcanvas-slide").hide();
-        }
-
-        function PrefixedEventAniEnd(element, callback) {
-            let fn = function () {
-                element.classList.add("uk-hidden");
-                for (let i=0; i<pfx.length; i++) {
-                    element.removeEventListener(pfx[i]+"animationend", fn);
-                }
-                callback();
-            };
-
-            for (let i=0; i<pfx.length; i++) {
-                element.addEventListener(pfx[i]+"animationend", fn, false);
-            }
         }
 
         window.onhashchange = locationSwitcher;
